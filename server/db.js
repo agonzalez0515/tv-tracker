a326002a4bc298b81651dfb2f40ac955a98db7f3
Angelica Gonzalez <angelica@8thlight.com>
2020-03-05 22:41:44 -0800

    Add sample grapql resolvers and db connection

---
+++ server/db.js
@@ -0,0 +1,13 @@
+const { Client } = require('pg')
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME
+})
+
+client.connect()
+
+module.exports = client 
