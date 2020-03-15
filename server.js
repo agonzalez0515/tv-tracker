a20b4ea1acdf51d48e49815be069089aa77b02ea
Angelica Gonzalez <angelica@8thlight.com>
2020-03-15 12:59:17 -0700

    Fix protected routes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const path = require("path");
 const { withAuth } = require("./utils/authHelpers");
 const schema = require("./api/schema");
 const app = require("./setupServer");
@@ -16,6 +18,13 @@ app.get("/logout", function(req, res) {
 app.get("/status", function(req, res) {
   res.status(200).json({ status: "I'm alive!" });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.use(withAuth);
 // resources api
--- setupServer.js
+++ setupServer.js
@@ -2,16 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const path = require("path");
 
 const app = express();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
 
 app.use(
   bodyParser.urlencoded({
