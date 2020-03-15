24f4374ee21d55f1e05eea4f48586f70b246e57a
Angelica Gonzalez <angelica@8thlight.com>
2020-03-15 01:16:53 -0700

    Update index html page title. Fix routing

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const path = require("path");
 const graphqlHTTP = require("express-graphql");
 const { withAuth } = require("./utils/authHelpers");
 const schema = require("./api/schema");
@@ -16,6 +18,13 @@ app.get("/logout", function(req, res) {
 app.get("/status", function(req, res) {
   res.status(200).json({ status: "I'm alive!" });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.use(withAuth);
 // resources api
--- setupServer.js
+++ setupServer.js
@@ -2,12 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const path = require("path");
 
 const app = express();
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client/build")));
+//   app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   });
+// }
 
 app.use(
   bodyParser.urlencoded({
